d7c32cd134ddb04e3232fc28cd006d9338cd23b8
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-10-03 13:45:28 +0300

    Initialized project, added assets and styles, installed npm packages

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Exo_2 } from "next/font/google";
+
+import "./globals.css";
+
+const exo2 = Exo_2({
+  variable: "--font-exo-2",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Photaland",
+    template: "%s | Photaland",
+  },
+  description:
+    "Discover Photaland Store – your one-stop shop for premium photo products and accessories. From custom photo books and wall art to frames, albums, and camera essentials, we provide high-quality items to preserve and showcase your memories beautifully.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${exo2.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
